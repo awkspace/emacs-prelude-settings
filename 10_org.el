e27977cc4aeca28eba653a48dcb6d6993e2457a0
awk <awk@shutupandwrite.net>
2016-11-30 15:42:03 -0800

    Add org-mode binds

---
+++ 10_org.el
@@ -0,0 +1,13 @@
+;;; 01_org.el --- org-mode
+
+;;; Commentary:
+;;; Binds/settings specific to Emacs org-mode.
+
+;;; Code:
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;; 01_org.el ends here
