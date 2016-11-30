984589b86997a2a459fd91e8d50dd9e6246a1e0b
awk <awk@shutupandwrite.net>
2016-11-30 15:42:48 -0800

    Set default theme: 'darkokai'

---
+++ preload/00_theme.el
@@ -0,0 +1,11 @@
+;;; 00_theme.el --- Theme configuration
+
+;;; Commentary:
+
+;; For setting theme variables before loading the rest of Prelude.
+
+;;; Code:
+
+(setq prelude-theme 'darkokai)
+
+;;; 00_theme.el ends here
