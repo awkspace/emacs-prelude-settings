419d90f23406e2c7351099c68d58db490f511385
awk <awk@shutupandwrite.net>
2018-01-05 20:07:51 -0500

    Use a cleaner method of matching Liquid comments' start/end

--- 10_markdown.el
+++ 10_markdown.el
@@ -17,30 +17,13 @@
 
   ;; TODO: Match HTML comments too for backwards compatibility.
 
-  (defun markdown-syntax-propertize-comments (start end)
-  "Match liquid comments from the START to END."
-  (let* ((in-comment (markdown-in-comment-p)))
-    (goto-char start)
-    (cond
-     ;; Comment start
-     ((and (not in-comment)
-           (re-search-forward "{%\\(?:\s*\\|\n\\)comment\\(?:\s*\\|\n\\)%}" end t)
-           (not (markdown-inline-code-at-point-p))
-           (not (markdown-code-block-at-point-p)))
-      (let ((open-beg (match-beginning 0)))
-        (put-text-property open-beg (1+ open-beg)
-                           'syntax-table (string-to-syntax "<"))
-        (markdown-syntax-propertize-comments
-         (min (1+ (match-end 0)) end (point-max)) end)))
-     ;; Comment end
-     ((and in-comment
-           (re-search-forward "{%\\(?:\s*\\|\n\\)endcomment\\(?:\s*\\|\n\\)%}" end t))
-      (put-text-property (1- (match-end 0)) (match-end 0)
-                         'syntax-table (string-to-syntax ">"))
-      (markdown-syntax-propertize-comments
-       (min (1+ (match-end 0)) end (point-max)) end))
-     ;; Nothing found
-     (t nil))))
+  (defconst markdown-regex-comment-start
+    "{%\\(?:\s*\\|\n\\)comment\\(?:\s*\\|\n\\)%}"
+    "Regular expression matches Liquid comment opening.")
+
+  (defconst markdown-regex-comment-end
+    "{%\\(?:\s*\\|\n\\)endcomment\\(?:\s*\\|\n\\)%}"
+    "Regular expression matches Liquid comment closing.")
 
 )
 
