07a093bf4f8e034c56cd3961e3fddfd9e0424b07
awk <awk@shutupandwrite.net>
2018-05-24 10:27:55 -0400

    Set initial state to 'emacs' when in a terminal

--- 10_shell.el
+++ 10_shell.el
@@ -1,2 +1,5 @@
 (add-hook 'eshell-mode-hook (lambda () (linum-mode -1)))
 (add-hook 'term-mode-hook (lambda () (linum-mode -1)))
+
+(evil-set-initial-state 'eshell-mode 'emacs)
+(evil-set-initial-state 'term-mode 'emacs)
